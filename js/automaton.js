5532e2408983c6d79c09ba17313e8adb16e9545f
lorossi <contact@lorenzoros.si>
2022-03-15 14:03:49 +0100

    commented code

--- js/automaton.js
+++ js/automaton.js
@@ -26,6 +26,9 @@ const absInOutEase = (x, ni = 5, no = 5) => {
  */
 const polyInEase = (x, n = 5) => Math.pow(x, n);
 
+/**
+ * Decimal - Hexadecimal conversion
+ */
 const dec_to_hex = (dec, padding = 0, prefix = false, round = true) => {
   if (round) dec = Math.floor(dec);
   let hex = dec.toString(16).padStart(padding, 0).toUpperCase();
@@ -37,10 +40,13 @@ class Automaton {
   constructor(size) {
     this._size = size;
     this._current_frame = 0;
+    this._ended = false;
+    this._error = false;
 
     const disk_width = this._size / TAPES_NUM / 2;
     const disk_spacing = disk_width / 2;
 
+    // instantiate all the K tapes
     this._tapes = Array(TAPES_NUM)
       .fill(null)
       .map((_, i) => {
@@ -49,30 +55,50 @@ class Automaton {
       })
       .reverse();
 
+    // instantiate the heads
     this._heads = Array(TAPES_NUM)
       .fill(null)
       .map((_, i) => new Head(this._tapes[i]._r, disk_width / 5));
 
+    // instantiate the FSA controlling the TM
     this._fsa = new FSA(this._size / 2);
   }
 
+  /**
+   * Set the tapes content
+   * @param {Array} contents: array of strings
+   */
   setTapes(contents) {
     contents.forEach((c, i) => this._tapes[i].setTape(c.split("")));
   }
 
+  /**
+   *
+   * @param {int} current_frame
+   */
   update(current_frame) {
+    if (this._error || this._ended) return;
+    // save the current frame, needed for animations
     this._current_frame = current_frame;
 
+    // update the FSA and each frame
     this._fsa.update(current_frame);
+    if (this._fsa.error) {
+      this._error = true;
+    }
     this._tapes.forEach((t) => t.update(current_frame));
 
-    if (
-      this._tapes.every(
-        (t) => !t.is_animating && !this._fsa.ended && !this._fsa.is_animating
-      )
-    ) {
-      const updates = this._fsa.step(this.current_chars);
+    // check if the fsa has ended
+    if (this._fsa.ended) {
+      this._ended = true;
+      return;
+    }
 
+    // check if all animations are not running
+    if (this._tapes.every((t) => !t.is_animating && !this._fsa.is_animating)) {
+      // step the FSA and get tape movements and new tapes characters
+      const updates = this._fsa.step(this.current_chars);
+      // there are updates, update each tape
       if (updates) {
         this._tapes.forEach((t, i) =>
           t.setAnimation(
@@ -85,6 +111,7 @@ class Automaton {
   }
 
   show(ctx) {
+    // show all tapes , heads and FSA
     ctx.save();
     this._tapes.forEach((d) => d.show(ctx));
     this._heads.forEach((h) => h.show(ctx));
@@ -92,17 +119,36 @@ class Automaton {
     ctx.restore();
   }
 
+  /**
+   * Get chars under each head as a string
+   * @returns String
+   */
   get current_chars() {
     return this._tapes.reduce((a, b) => a + b.getCurrentChar().toString(), "");
   }
 
+  /**
+   * Set chars under each head as a string
+   * @param {String} chars
+   */
   set current_chars(chars) {
     chars.forEach((c, i) => {
       this._tapes[i].current_char = c;
     });
   }
+
+  get error() {
+    return this._error;
+  }
+
+  get ended() {
+    return this._ended;
+  }
 }
 
+/**
+ * Class handling a single state
+ */
 class State {
   constructor(name, initial = false, final = false) {
     this.name = name;
@@ -111,6 +157,9 @@ class State {
   }
 }
 
+/**
+ * Class handling transitions between states
+ */
 class Transition {
   constructor(from_state, to_state, chars, new_chars, directions) {
     this.from_state = from_state;
@@ -124,48 +173,56 @@ class Transition {
 class FSA {
   constructor(size) {
     this._size = size;
-    this._ended = false;
+    this._ended = false; // automaton has stopped after having reached error or end state
+    this._error = false; // automaton has reached error state
     this._animation_started = 0;
     this._is_animating = false;
+    // opacity relative to the current state
+    this._current_opacity = 255;
 
+    // Initialize all the states
     this._states = [
       new State("q0", true, false),
       new State("q1", false, false),
       new State("q2", false, true),
     ];
+    // Initialize all the transitions
     this._transitions = [
       new Transition("q0", "q0", "0  ", "0  ", "RSS"),
       new Transition("q0", "q0", "1  ", "1  ", "RSS"),
-
       new Transition("q0", "q1", "   ", " 0 ", "LSS"),
-
       new Transition("q1", "q1", "10 ", "100", "LSL"),
       new Transition("q1", "q1", "00 ", "001", "LSL"),
-
       new Transition("q1", "q2", " 0 ", "   ", "SSS"),
     ];
-
+    // select the initial state
     this._current_state = this._states.filter((s) => s.initial)[0].name;
-    this._current_opacity = 255;
   }
 
   step(chars) {
+    // don't step if the animation is not yet ended
     if (this._ended || this._is_animating) return;
-
+    // filter all the transitions having the current state as from state
     const transitions = this._transitions.filter(
       (t) => t.from_state == this._current_state && t.chars == chars
     );
-
-    if (transitions.length != 1) return;
-
+    // no transitions found! exit
+    if (transitions.length != 1) {
+      this._ended = true;
+      this._error = true;
+      return;
+    }
+    // update the current state
     this._current_state = transitions[0].to_state;
 
+    // a final state has been reached
     if (
       this._states.filter((s) => this._current_state == s.name && s.final)
-        .length > 0
+        .length == 1
     )
       this._ended = true;
 
+    // set the animation to true
     this._is_animating = true;
 
     return {
@@ -176,23 +233,28 @@ class FSA {
 
   update(current_frame) {
     if (!this._is_animating) {
+      // this is quite hacky, but keep setting the starting frame as the next
+      // as long as there is no animation
       this._animation_started = current_frame + 1;
       return;
     }
 
+    // calculate elapsed time from animation start
     const elapsed = current_frame - this._animation_started;
-
+    // end animation if enough time has been elapsed
     if (elapsed > ANIMATION_DURATION) {
       this._is_animating = false;
       return;
     }
 
+    // calculate current state opacity
     const percent = elapsed / ANIMATION_DURATION;
     this._current_opacity = polyInEase(percent, 4) * 255;
   }
 
   show(ctx) {
     const theta = (Math.PI * 2) / this._states.length;
+    // there's a lower bound on the state size
     const state_size = Math.min(
       this._size / 10,
       (Math.PI * 2 * this._size) / 2 / this._states.length
@@ -206,12 +268,13 @@ class FSA {
     ctx.stroke();
 
     ctx.save();
-    this._states.forEach((s, i) => {
+    this._states.forEach((s) => {
+      // draw each state around the circle
       ctx.rotate(theta);
       ctx.beginPath();
       ctx.arc(0, this._size / 3, state_size, 0, Math.PI * 2);
       ctx.stroke();
-
+      // if this is the current state, fill it opaquely
       if (s.name == this._current_state) {
         const hex_opacity = dec_to_hex(this._current_opacity);
         ctx.fillStyle = WHITE + hex_opacity;
@@ -229,6 +292,11 @@ class FSA {
 
   get ended() {
     return this._ended;
+  }
+
+  get error() {
+    // is the FSA in an error state?
+    return this._error;
   }
 }
 
@@ -255,15 +323,17 @@ class Head {
   }
 }
 
+/** Class handling the tape */
 class CircularTape {
   constructor(r, width) {
     this._r = Math.floor(r);
     this._inner_r = Math.floor(r - width);
 
+    // fill the tape with blank characters
     this._tape = Array(TAPE_LEN)
       .fill(null)
       .map((_) => ALPHABET[ALPHABET.length - 1]);
-
+    // convert directions in formal way to direction
     this._directions_map = { R: 1, L: -1, S: 0 };
 
     this._current_frame = 0;
@@ -281,12 +351,14 @@ class CircularTape {
   }
 
   setTape(tape) {
-    this._tape = [...tape].fill(" ", tape.length, TAPE_LEN);
+    // copy the tapes, filling with enough spaces
+    this._tape = [...tape];
     for (let i = tape.length; i < TAPE_LEN; i++)
       this._tape.push(ALPHABET[ALPHABET.length - 1]);
   }
 
   setAnimation(new_char, step_direction) {
+    // set the animation with the new char and the step direction
     this._is_animating = true;
     this._animation_started = this._current_frame;
     this._new_char = new_char;
@@ -299,13 +371,16 @@ class CircularTape {
     if (this._is_animating) {
       const diff = this._current_frame - this._animation_started;
       const percent = (diff / ANIMATION_DURATION) % 1;
+
       if (diff < ANIMATION_DURATION) {
+        // animate the char
         if (this._new_char != this.getCurrentChar()) {
           this.setCurrentChar(this._new_char);
           this._char_changed = true;
         }
         this._current_opacity = polyInEase(percent) * 255;
       } else if (diff < 2 * ANIMATION_DURATION) {
+        // animate the rotation
         this._current_rotation =
           ((absInOutEase(percent, 4, 10) * Math.PI * 2) / TAPE_LEN) *
           this._step_direction;
